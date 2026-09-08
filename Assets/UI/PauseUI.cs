8b0b3d0bc24a8880e3c0c461383ee93d9c80643f
camipolitis <caypova@gmail.com>
2026-09-08 02:05:24 -0300

    UI/codewiring-08092026-020505

--- Assets/UI/PauseUI.cs
+++ Assets/UI/PauseUI.cs
@@ -1,6 +1,19 @@
+using Enigma.Core;
+using Enigma.Player;
 using UnityEngine;
 
-public class PauseUI
+namespace Enigma.UI
 {
-    
+    // Escucha Pause del input y delega en PauseSystem.
+    public class PauseUI : MonoBehaviour
+    {
+        [SerializeField] private PlayerInputHandler input;
+        [SerializeField] private PauseSystem pauseSystem;
+
+        private void Update()
+        {
+            if (input != null && input.PausePressedThisFrame)
+                pauseSystem?.Toggle();
+        }
+    }
 }
